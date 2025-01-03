e4e30afd09842feea2ff0cd4dd02ac5529d56461
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2025-01-03 09:44:48 +0900

    商品一覧取得APIの作成

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-fleamarket/controllers"
+	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	items := []models.Item{
+		{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
+		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
+		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
 	router := gin.Default()
-	router.GET("/sample", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run()
+	router.GET("/items", itemController.FindAll)
+	router.Run("localhost:8080")
 }
