a1bd93ec2eeb2142fb1e44781b20c995b6804889
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2025-03-30 08:44:04 +0900

    異常系APIのテスト

--- main_test.go
+++ main_test.go
@@ -109,3 +109,28 @@ func TestCreate(t *testing.T) {
 	assert.Equal(t, http.StatusCreated, w.Code)
 	assert.Equal(t, uint(4), res["data"].ID)
 }
+
+func TestCreateUnauthorized(t *testing.T) {
+	// テストのセットアップ
+	router := setup()
+
+	createItemInput := dto.CreateItemInput{
+		Name:        "テストアイテム4",
+		Price:       4000,
+		Description: "Createテスト",
+	}
+	reqBody, _ := json.Marshal(createItemInput)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/v1/items", bytes.NewBuffer(reqBody))
+
+	// APIリクエストの実行
+	router.ServeHTTP(w, req)
+
+	// APIの実行結果を取得
+	var res map[string]models.Item
+	json.Unmarshal([]byte(w.Body.String()), &res)
+
+	// アサーション
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
