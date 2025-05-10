6a686bcf6ffe958a8ceca5e7f6a7c2b6943a8698
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2025-05-10 09:21:08 +0900

    ホットリロードの実装

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
+	router.GET("/sample", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
