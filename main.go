d54a231250ea9ba6403f0788ac62f89f7db1cb23
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2025-06-14 12:32:20 +0900

    ルーターのグルーピング

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"gin-fleamarket/controllers"
 	"gin-fleamarket/infra"
+
 	// "gin-fleamarket/models"
 	"gin-fleamarket/repositories"
 	"gin-fleamarket/services"
@@ -25,10 +26,11 @@ func main() {
 	itemController := controllers.NewItemController(itemService)
 
 	r := gin.Default()
-	r.GET("/items", itemController.FindAll)
-	r.GET("/items/:id", itemController.FindById)
-	r.POST("/items", itemController.Create)
-	r.PUT("/items/:id", itemController.Update)
-	r.DELETE("/items/:id", itemController.Delete)
+	itemRouter := r.Group(("/items"))
+	itemRouter.GET("", itemController.FindAll)
+	itemRouter.GET("/:id", itemController.FindById)
+	itemRouter.POST("", itemController.Create)
+	itemRouter.PUT("/:id", itemController.Update)
+	itemRouter.DELETE("/:id", itemController.Delete)
 	r.Run("localhost:8080")
 }
